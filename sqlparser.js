e7bf5177940c2957567c8044e70a8d35ac9f6688
Henrik Korsgaard <korsgaard@cs.au.dk>
2022-11-23 14:08:59 +0100

    added csv skeleton support

--- sqlparser.js
+++ sqlparser.js
@@ -1,6 +1,4 @@
 
-const reservedWords = ["SELECT","FROM", "WHERE", ">=", "<=", "!=", ",", "=", ">", "<", "(", ")"]
-const steps = ["stepType", "stepSelectField", "stepSelectFrom","stepSelectComma","stepSelectFromTable", "stepWhere", "stepWhereField", "stepWhereOperator","stepWhereValue", "stepWhereAnd"]
 
 const indentifierRegExp = new RegExp('[a-zA-Z0-9_*]'); 
 
@@ -11,20 +9,69 @@ class SQLException {
     }
 }
 
-class Condition {
-    
-    static Unknown = -1
-    static Eq = 0
-    static Ne = 1
-    static Gt = 2
-    static Lt = 3
-    static Gte = 4
-    static Lte = 5
+/*
+Without any help:
+    - SELECT name FROM master.sys.databases 
+	- SQL: SELECT * FROM tables;
+	- SQL: SELECT * FROM Information_SCHEMA.colums WHERE tableName = 'tablename';
+	- SQL: SELECT * FROM table; become an instrument for data discovery
 
-    constructor(firstCondition){
-        this.firstCondition = firstCondition
-        this.secondCondition = ""
-        this.operator = 0
+*/
+
+/*
+
+Databases
+- name //Library
+- tables //Books //Authors //Publishers
+    - name
+    - columns
+
+- name //Movies
+- tables //Movies //Actors //Directors
+
+*/
+
+let master = { "databases": []}
+
+
+class Condition {
+
+    //SQL interprets string comparisons (<,>, <=, >=) different than Javascript.
+    //SQL compare by alphabetical order,  JavaScript by string lenght. 
+    //TODO: This can be fixed by using typecheck (ugh) and sort in the relevant funcitons
+    static operations = {
+        "Eq":{
+            "SQL": "=",
+            "f":function (a, b) { return a == b }
+        },
+        "Ne":{
+            "SQL":"!=",
+            "f":function (a, b) { return a != b },
+        },
+        "Gt":{
+            "SQL": ">",
+            "f":function (a, b) { return a > b },
+        },
+        "Lt":{
+            "SQL": "<",
+            "f":function (a, b) { return a < b },
+        },
+        "Gte":{
+            "SQL": ">=",
+            "f":function (a, b) { return a >= b },
+        },
+        "Lte":{
+            "SQL": "<=",
+            "f":function (a, b) { return a <= b },
+        }
+    }
+
+    static supportedOperations = [">=", "<=", "!=", "=", ">", "<"]
+
+    constructor(field){
+        this.field = field
+        this.data = ""
+        this.operatorFunction = null 
     }
 }
 
@@ -38,143 +85,198 @@ class Query {
 }
 
 class SQLParser {
+    static KEYWORDS = ["SELECT","FROM", "WHERE",",",">=", "<=", "!=", "=", ">", "<"]
+    static steps = ["stepType",
+    "stepSelectField",
+    "stepSelectComma",
+    "stepSelectFrom",
+    "stepSelectFromTable",
+    "stepWhere",
+    "stepWhereField",
+    "stepWhereOperator",
+    "stepWhereValue",
+    "stepWhereAnd"]
     constructor(){
-        this.index = 0
-        this.sql = ""
-        this.step = 0
-        this.query = new Query()
-        this.err = null
     }
 
     /*
         TODO:
-        - ERROR HANDLING
         - RETURN QUERY AT EACH STEP
+            - ah, this became a rabbit hole
+            - import from csv -> table and sql'able object
+            - then we need basic database and table meta data to support poking query
+            - ... and then we are set to do something interesting.
+
+        - VALIDATION
+        - COVERAGE
+        - TESTS?
     */
+
+    generateAutoCompleteSuggestions(table, fieldsArray, fieldsValueObject){
+        keywordThree = [
+            {
+                "step": SQLParser.steps.indexOf("stepType"), 
+                "options": ["SELECT"]
+            },
+            {
+                "step": SQLParser.steps.indexOf("stepSelectField"),
+                "options": [...fieldsArray, "*"]
+            },
+            {
+                "step": SQLParser.steps.indexOf("stepSelectComman"),
+                "options": [","]
+            },
+            {
+                "step": SQLParser.steps.indexOf("stepSelectFrom"),
+                "options": ["FROM"]
+            },
+            {
+                "step": SQLParser.steps.indexOf("stepSelectFromTable"),
+                "options": [table]
+            },
+            {
+                "step": SQLParser.steps.indexOf("stepSelectWhere"),
+                "options": ["WHERE"]
+            },
+            {
+                "step": SQLParser.steps.indexOf("stepSelectField"),
+                "options": fields
+            },
+            {
+                "step": SQLParser.steps.indexOf("stepSelectOperator"),
+                "options": supportedOperations
+            },
+            //Note, we cannot sugget any values for the step: stepWhereValue, without having analysed the entire database.
+            //Might be interesting for a toy example -- lets try
+            {
+                "step": SQLParser.steps.indexOf("stepSelectOperator"),
+                "options": supportedOperations
+            },
+           
+            {
+                "step": SQLParser.steps.indexOf("stepSelectWhereAnd"),
+                "options": ["AND"]
+            },
+        ]
+    }
+   
 
     parse(sql){
         this.sql = sql
+        this.index = 0
+        this.step = 0
+        this.query = new Query()
+
         if(this.sql[this.sql.length-1] != ";"){
             throw new SQLException("SQL statement does not end with a ;")
-            return
         }
-        let run = true
-        while (run) {        
+        while (true){        
             if(this.sql[this.index] == ";") {
                 return this.query
             }
-            switch(steps[this.step]) {
+            switch(SQLParser.steps[this.step]) {
                 case "stepType":
                     switch(this.peek().toUpperCase()){
                         case "SELECT":
                             this.query.type = "SELECT"
                             this.pop()
-                            this.step = steps.indexOf("stepSelectField")
+                            this.step = SQLParser.steps.indexOf("stepSelectField")
                             break;
                         default: 
-                            console.log("we need to throw an error here")
-                            run = false
-                            return 
+                            throw new SQLException("Invalid query type")
                     }
                     break;
                 case "stepSelectField":
                     let identifier = this.peek()
-                    // the other library does an additional check here, but I don't know why
                     this.query.fields.push(identifier)
                     this.pop()
                     // Additional step to implement "AS" do later
                     if(this.peek().toUpperCase() == "FROM"){
-                        this.step = steps.indexOf("stepSelectFrom")
+                        this.step = SQLParser.steps.indexOf("stepSelectFrom")
                         break
                     }
-                    this.step = steps.indexOf("stepSelectComma")
+                    this.step = SQLParser.steps.indexOf("stepSelectComma")
                     break;
                 case "stepSelectComma":
                     let comma = this.peek()
                     if(comma != ","){
-                        run = false
-                        console.log("Error here because we did not get comma and next word cannot be keyword because we already skipped that step above. Hence, an error is needed")
+                        throw new SQLException(`Invalide query: ${this.sql}. Something went wrong around string index ${this.index}: ${this.sql.substring(0, this.index)}! Expected a comma or keyword.`)
                     }
                     this.pop()
-                    this.step = steps.indexOf("stepSelectField")
+                    this.step = SQLParser.steps.indexOf("stepSelectField")
                     break
                 case "stepSelectFrom":
                     let from = this.peek().toUpperCase()
                     if(from != "FROM"){
-                        console.log("this should never happen")
-                        run = false
-                        break
+                        throw new SQLException(`Invalide query: ${this.sql}. Something went wrong around string index ${this.index}: ${this.sql.substring(0, this.index)}! Expected keyword FROM.`)
                     }
                     this.pop()
-                    this.step = steps.indexOf("stepSelectFromTable")
+                    this.step = SQLParser.steps.indexOf("stepSelectFromTable")
                     break
                 case "stepSelectFromTable":
                     let tableName = this.peek()
                     if(tableName.length == 0){
-                        console.log("errorhandling is for later, eh")
-                        run = false
-                        break;
+                        throw new SQLException(`Invalide query: ${this.sql}. Something went wrong around string index ${this.index}: ${this.sql.substring(0, this.index)}! Expected tableName returned empty.`)
                     }
                     this.query.tableName = tableName
                     this.pop()
-                    this.step = steps.indexOf("stepWhere")
+                    this.step = SQLParser.steps.indexOf("stepWhere")
                     break
                 case "stepWhere":
                     let whereWord = this.peek()
                     if(whereWord.toUpperCase() != "WHERE"){
-                        console.log("again, a very unlikely that this is not WHERE")
-                        run = false
-                        break
+                        throw new SQLException(`Invalide query: ${this.sql}. Something went wrong around string index ${this.index}: ${this.sql.substring(0, this.index)}! Expected a WHERE clause (or ;)`)
                     }
                     this.pop()
-                    this.step = steps.indexOf("stepWhereField")
+                    this.step = SQLParser.steps.indexOf("stepWhereField")
                     break
                 case "stepWhereField": {
-                    let identifier = this.peek()
-                    let condition = new Condition(identifier)
+                    let field = this.peek()
+                    //We assume that all conditions are given field first (field = condition)
+                    // condition = field is also valid in SQL, but not supported in this toy example
+                    let condition = new Condition(field)
                     this.query.conditions.push(condition)
                     this.pop()
-                    this.step = steps.indexOf("stepWhereOperator")
+                    this.step = SQLParser.steps.indexOf("stepWhereOperator")
                     break
                     }
                 case "stepWhereOperator":
                     let operator = this.peek()
-                    let currentCondition = this.query.conditions[this.query.conditions.length-1]
+                    let curCondition = this.query.conditions[this.query.conditions.length-1]
                     switch(operator){
                         case "=":
-                            currentCondition.operator = Condition.Eq
+                            curCondition.operator = Condition.operations["Eq"]
                             break
                         case ">":
-                            currentCondition.operator = Condition.Gt
+                            curCondition.operator = Condition.operations["Gt"]
                             break
                         case ">=":
-                            currentCondition.operator = Condition.Gte
+                            curCondition.operator = Condition.operations["Gte"]
                             break
                         case "<":
-                            currentCondition.operator = Condition.Lt
+                            curCondition.operator = Condition.operations["Lt"]
                             break
                         case "<=":
-                            currentCondition.operator = Condition.Lte
+                            curCondition.operator = Condition.operations["Lte"]
                             break
                         case "!=":
-                            currentCondition.operator = Condition.Ne
+                            curCondition.operator = Condition.operations["Ne"]
                             break
                         default:
-                            console.log("another error")
-                            run = false
+                            throw new SQLException(`Invalide query: ${this.sql}. The provided condition (${condition}) is not supported!`)
                             break
                     }
-                    this.query.conditions[this.query.conditions.length-1] = currentCondition
+                    this.query.conditions[this.query.conditions.length-1] = curCondition
                     this.pop()
-                    this.step = steps.indexOf("stepWhereValue")
+                    this.step = SQLParser.steps.indexOf("stepWhereValue")
                     break
                 case "stepWhereValue": { 
-                        let identifier = this.peek()
-                        let currentCondition = this.query.conditions[this.query.conditions.length-1]
-                        currentCondition.secondCondition = identifier
-                        this.query.conditions[this.query.conditions.length-1] = currentCondition
+                        let data = this.peek()
+                        let curCondition = this.query.conditions[this.query.conditions.length-1]
+                        curCondition.data = data
+                        this.query.conditions[this.query.conditions.length-1] = curCondition
                         this.pop()
-                        this.step = steps.indexOf("stepWhereAnd")
+                        this.step = SQLParser.steps.indexOf("stepWhereAnd")
                         break;
                     }
                 case "stepWhereAnd":
@@ -183,25 +285,26 @@ class SQLParser {
                         console.log("error must handle")
                     }
                     this.pop()
-                    this.step = steps.indexOf("stepWhereField")
+                    this.step = SQLParser.steps.indexOf("stepWhereField")
                     break;
                 default:
-                    console.log("we need another error here")
-                    run = false
+                    throw new SQLException(`Critical: Unknown SQL error on ${this.sql}. Expected statement termination or something else.`)
                     return
             }
         }
     }
+
    
     peek() {
         return this.peekCases()[0]
     }
 
     pop(){
+        //We pop via the index and not on the original SQL statement
         let peekedLength = this.peekCases()
         let peeked = peekedLength[0]
         this.index += peekedLength[1]
-        //deal with  the whitespace
+        //Here we deal with the whitespace
         for(;this.index < this.sql.length && this.sql[this.index] == ' '; this.index++){}
         return
     }
@@ -211,8 +314,8 @@ class SQLParser {
             return ["",0]
         }
         
-        for(var i = 0; i < reservedWords.length; i++){
-            let w = reservedWords[i]
+        for(var i = 0; i < SQLParser.KEYWORDS.length; i++){
+            let w = SQLParser.KEYWORDS[i]
             
             let token = this.sql.substring(this.index,this.index + w.length)
             if(w == token){
@@ -241,18 +344,6 @@ class SQLParser {
         
         return [identifier, identifier.length]
     }
-    /*
-    func (p *parser) peekQuotedStringWithLength() (string, int) {
-        if len(p.sql) < p.i || p.sql[p.i] != '\'' {
-            return "", 0
-        }
-        for i := p.i + 1; i < len(p.sql); i++ {
-            if p.sql[i] == '\'' && p.sql[i-1] != '\\' {
-                return p.sql[p.i+1 : i], len(p.sql[p.i+1:i]) + 2 // +2 for the two quotes
-            }
-        }
-        return "", 0
-    }*/
 
     peekQuotedString(){
         if(this.sql.length < this.index || this.sql[this.index] != '\''){
@@ -269,5 +360,37 @@ class SQLParser {
         return ["",0]
     }
 
+    validate(){
+        //Consider implementing validation (per golang example) if needing something more elaborate
+    }
 }
+
+class CSVFileParser {
+    constructor(csvfile){
+        //This requires running python3 -m http.server
+        let xhr = new XMLHttpRequest()
+
+        xhr.onload = function(){
+            let data = xhr.responseText
+            let rows = data.split(/\r?\n|\r/);
+            //first row should be the table colums
+            //rest is data.
+            
+            console.log(xhr.responseText)
+        }
+
+        xhr.open("get", "http://localhost:8000/books.csv", true)   
+        xhr.send()
+    }
+}
+
+class CSVToSQLTable {
+    //For querying on the side
+    //Including the model
+    constructor(csvfile){
+
+    }
+}
+
+
 
